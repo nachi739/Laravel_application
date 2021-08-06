826af9ec8e2d9ecf298db28691ac88d8bbb0bfff
nachi739 <namiki081025@gmail.com>
2021-08-06 05:42:47 +0000

    blank

--- app/User.php
+++ app/User.php
@@ -32,7 +32,7 @@ class User extends Authenticatable
         return $this->hasMany(Movie::class);
     }
     
-        public function followings()
+    public function followings()
     {
         return $this->belongsToMany(User::class, 'user_follow', 'user_id', 'follow_id')->withTimestamps();
     }
